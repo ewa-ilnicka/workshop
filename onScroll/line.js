109735093e7449fcc34caf52da5421ffdc4e0c18
ewa-ilnicka <ewa.h.ilnicka@gmail.com>
2021-01-09 17:40:09 +0100

    Line grows on scroll

--- onScroll/line.js
+++ onScroll/line.js
@@ -4,6 +4,7 @@ const div = document.createElement('div');
 document.body.appendChild(div);
 
 let size = 100;
+let grow = true;
 
 div.style.position = "fixed";
 div.style.top = 0;
@@ -11,3 +12,21 @@ div.style.left = 0;
 div.style.width = "100%";
 div.style.height = size + "px";
 div.style.backgroundColor = "green";
+
+const changeHeight = function () {
+
+    if (size > window.innerHeight / 2) {
+        grow = false;
+    } else if (size <= 0) {
+        grow = true;
+    }
+
+    if (grow == true) {
+        size += 10;
+    } else {
+        size -= 10;
+    }
+    div.style.height = size + "px";
+}
+
+window.addEventListener("scroll", changeHeight);

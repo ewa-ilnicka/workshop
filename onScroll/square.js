fe8d29464693503356094a871551e0263b30035a
ewa-ilnicka <ewa.h.ilnicka@gmail.com>
2021-01-09 17:11:01 +0100

    Added function on scroll

---
+++ onScroll/square.js
@@ -0,0 +1,31 @@
+const square = document.createElement('div');
+document.body.appendChild(square);
+
+let grow = true;
+
+let size = 100;
+square.style.width=size + "px";
+square.style.height= size +"px";
+
+window.addEventListener("scroll", function() {
+
+    if (grow == true) {
+        size += 5;
+        square.style.width = size + "px";
+        square.style.height = size + "px";
+    }
+
+    else {
+        size -= 5;
+        square.style.width = size + "px";
+        square.style.height = size + "px";
+    }
+
+    if (size >= window.innerWidth / 2) {
+    grow = !grow;
+    }
+    else if (size <= 0){
+        grow = true;
+    }
+ 
+});

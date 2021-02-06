b31af479e8c0985085d615fb84cd2b68c7e62293
ewa-ilnicka <ewa.h.ilnicka@gmail.com>
2021-02-06 12:47:55 +0100

    Added function for

---
+++ onClick/growing.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector("button");
+const items = document.querySelectorAll("li");
+let fontSize = 10;
+
+btn.addEventListener("click", () => {
+    console.log("działa");
+    fontSize++;
+    for (let i = 0; i < items.length; i++) {
+        items[i].style.display = "block";
+        items[i].style.fontSize = `${fontSize}px`;
+    }
+})
